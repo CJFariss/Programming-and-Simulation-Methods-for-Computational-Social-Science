f2040648864c568bea79af32d7ba37edcbb19b4e
Christopher Fariss <cjf0006@gmail.com>
2024-07-22 11:43:28 +0100

    added day05 programs

--- R-Program-Lessons/Day05_Measurement_RSTAN/RSTAN_dyanamic_binary_IRT_NAs_simulation.R
+++ R-Program-Lessons/Day05_Measurement_RSTAN/RSTAN_dyanamic_binary_IRT_NAs_simulation.R
@@ -1,10 +1,10 @@
 ## RSTAN.binary_IRT_NAs_simulation.r
 ##########################################################################
 ## INSTRUCTOR: Christopher Fariss
-## COURSE NAME: Advanced Computational Methods for Social Media and Textual Data (3B)
-## University of Essex Summer School 2023
+## COURSE NAME: Programming and Simulation Methods for Computational Social Science (2W)
+## University of Essex Summer School 2024
 ##
-## Date: 2023-08-11
+## Date: 2024-07-22
 ##
 ## Please e-mail me if you find any errors or have and suggestions
 ## e-mail: cjf0006@gmail.com
