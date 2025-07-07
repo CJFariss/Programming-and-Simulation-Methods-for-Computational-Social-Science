f0c138d8372a23f79d820722b00a0bb4a6c17efb
Christopher Fariss <cjf0006@gmail.com>
2025-07-07 16:53:07 +0100

    Update R_Demo_Intro_Program_Challenge_two_sum.R

--- R-Program-Lessons/Day01_Intro_programs/R_Demo_Intro_Program_Challenge_two_sum.R
+++ R-Program-Lessons/Day01_Intro_programs/R_Demo_Intro_Program_Challenge_two_sum.R
@@ -2,7 +2,7 @@
 ##########################################################################
 ## INSTRUCTOR: Christopher Fariss
 ## COURSE NAME: Programming and Simulation Methods for Computational Social Science (1T, 2W)
-## University of Essex Summer School 2024
+## University of Essex Summer School 2025
 ##
 ## Date: 2025-07-07
 ##
@@ -37,4 +37,4 @@
 ##
 ##########################################################################
 
-## (1) create a vector of integers numbers and a scalar integer target
+
