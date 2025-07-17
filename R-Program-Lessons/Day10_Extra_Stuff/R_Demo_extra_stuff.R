be4476db25cd233f6030f90a96b15fc9a3699fb3
Christopher Fariss <cjf0006@gmail.com>
2025-07-17 15:52:44 +0100

    Create R_Demo_extra_stuff.R

---
+++ R-Program-Lessons/Day10_Extra_Stuff/R_Demo_extra_stuff.R
@@ -0,0 +1,97 @@
+## R_Demo_extra_stuff.R
+##########################################################################
+## INSTRUCTOR: Christopher Fariss
+## COURSE NAME:
+##
+##
+## Week 7-17-2025
+##
+## Please e-mail me if you find any errors or have and suggestions (either email is fine)
+## e-mail: cjf0006@gmail.com
+## e-mail: cjfariss@umich.edu
+##########################################################################
+##
+## tidyverse website:
+## https://www.tidyverse.org/
+##
+##
+## tidyverse pipe operator:
+## https://style.tidyverse.org/pipes.html
+## https://r4ds.had.co.nz/pipes.html
+##
+##########################################################################
+## Introduction to tutorial:
+## For this R tutorial, we will learn:
+##
+## (1a) learn about the "pipe" operator |> for base R (this is only available in R versions 4.0 or greater)
+## (1b) learn about the "pipe" operator %>% for the tidyverse 
+##
+##########################################################################
+
+library(tidyverse)
+
+## set the number of rows and columns for the plot
+par(mfrow=c(3,2), mar=c(4,4,1,1))
+
+
+SIMS <- 500
+
+##########################################################################
+## base R examples
+##########################################################################
+
+## simulate two variables and storing 2 variables in a matrix
+x <- matrix(NA, ncol=2, nrow=SIMS)
+dim(x)
+x[,1] <- rnorm(SIMS)
+x[,2] <- rnorm(SIMS)
+## plot the 2 columns in the x matrix
+plot(x, col=2)
+
+## more compact version of simulating and storing 2 variables in a matrix
+x <- matrix(rnorm(SIMS*2), ncol=2, nrow=SIMS)
+
+## plot the 2 columns in the x matrix
+plot(x, col=3)
+
+
+##########################################################################
+## base R examples using the base pipe operator
+##########################################################################
+
+## using the base pipe operator
+x <- rnorm(SIMS) |>
+  matrix(ncol = 2)
+x <- rnorm(SIMS) |> matrix(ncol = 2)
+
+## plot the 2 columns in the x matrix
+plot(x, col=4)
+
+## do everything at once and don't save the x matrix using the base pipe operator
+rnorm(SIMS) |>
+  matrix(ncol = 2) |>
+  plot(col=1) 
+
+
+##########################################################################
+## base R examples using the tidyverse pipe operator
+##########################################################################
+
+## using the tidyverse operator
+x <- rnorm(SIMS) %>%
+    matrix(ncol = 2)
+## plot the 2 columns in the x matrix
+plot(x, col=5)
+
+## do everything at once and don't save the x matrix using the tidyverse pipe operator
+rnorm(SIMS) %>%
+  matrix(ncol = 2) %>%
+  plot(col=6) 
+
+
+x <- rnorm(SIMS)
+x <-  matrix(x, ncol = 2)
+plot(x, col=6) 
+
+
+
